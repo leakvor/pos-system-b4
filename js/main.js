b6c5b5c8655acde984263951bfc452f2ecb7b2ec
VORLEAK.KUN <VORLEAK.KUN@PNC.LAN>
2023-12-13 09:45:34 +0700

    #3 create daseboard page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openSlideMenu() {
+    document.getElementById('menu').style.width='250px';
+    document.getElementById('navigation').style.marginLeft='190px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('menu').style.width='0px';
+    document.getElementById('navigation').style.marginLeft='0px'; 
+}
